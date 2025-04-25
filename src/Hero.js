9ad8169f6b2e64ad3133ebdb9a60fc2aa59ff28a
marwanEsm <marwan.esmaail85@gmail.com>
2025-04-25 21:46:43 +0200

    update code

--- src/Hero.js
+++ src/Hero.js
@@ -3,8 +3,8 @@ import { addPlatform, platforms } from "./platform.js";
 const spriteSheet = new Image();
 spriteSheet.src = "../assets/imgs/spritesheet6.png";
 
-const frameWidth = 64;
-const frameHeight = 64;
+const frameWidth = 64; // width of one frame
+const frameHeight = 64; // height of one frame
 let totalFrames = 0;
 let currentFrame = 0;
 let x = platforms.length === 2 ? 200 : animateHero();
@@ -34,9 +34,15 @@ function fall(targetDistance) {
 }
 
 export function stop(sticks) {
-  if (sticks.length === 0) return;
+
+
+  if (sticks.length === 0) {
+    /*  console.error("No sticks available to check height."); */
+    return; // Exit the function if no sticks are present
+  }
 
   const currentPlatform = platforms[targetPlatformIndex - 1];
+
   const nextPlatform = platforms[targetPlatformIndex];
   const lastStick = sticks[sticks.length - 1];
 
@@ -65,19 +71,24 @@ export function stop(sticks) {
 export function animateHero(ctx) {
   ctx.drawImage(
     spriteSheet,
-    currentFrame * frameWidth,
-    y,
-    frameWidth,
-    frameHeight,
-    x,
-    yCanvas,
-    frameWidth,
-    frameHeight
+
+    currentFrame * frameWidth, // source x from the spritesheet : where to start the x position every frame
+    y, // source y from the spritesheet :  y position every frame, here is 0 always top left, just one row
+
+    frameWidth, /// source width in the spritesheet
+    frameHeight, // source height in spritesheet
+
+    x, /// position x on canvas
+    yCanvas, /// position y in canvas, it the rowanimation number in case there are more rows
+
+    frameWidth, // width on canvas
+    frameHeight // height in canvas
   );
 
   if (isMoving) {
     if (totalFrames % 4 === 0) {
-      currentFrame = (currentFrame + 1) % 6;
+      // Adjust the frame delay as needed
+      currentFrame = (currentFrame + 1) % 6; // Cycle through 6 frames (0 to 5)
     }
 
     totalFrames++;
